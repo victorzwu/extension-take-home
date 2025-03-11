9c8c1035333841504ed9d4b51b5c2873967c36c9
Victor Wu <victor.wu08@gmail.com>
2025-03-11 12:14:52 -0700

    specify config file input/output

--- victor-wu-extension/vite.config.ts
+++ victor-wu-extension/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from 'vite';
+import preact from '@preact/preset-vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
-})
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        popup: 'public/popup.html'
+      },
+      output: {
+        entryFileNames: '[name].js'
+      }
+    }
+  }
+});
